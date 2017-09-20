3e118bc5961b74094f7691b7708bea0ef6971206
David Cisneros <d.cisneros849@gmail.com>
2017-09-20 01:14:52 -0400

    put together configuration of twitter bot

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: '	52nmL0OL2jKtao189uOqaxihT',
+  consumer_secret: 'vR7sPL4QOew0M9dYiQEpbdELXJdx71lztaOIKeLjJOFgzsf4Tg',
+  access_token_key: '724883174694727680-ideXHlM7nMTzKXRsi2JFM9IErc5m0fL',
+  access_token_secret: 'wQ6ixtEi8ZKyDCVXqMdAL1FOUZcw5HcgzTpUMxFrdPYpt'
+}
