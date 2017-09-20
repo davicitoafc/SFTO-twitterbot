1fcfa10a5c0827e562ab57e516919488beae1053
David Cisneros <d.cisneros849@gmail.com>
2017-09-20 11:56:54 -0400

    updated code

--- app.js
+++ app.js
@@ -12,8 +12,26 @@ var params = {
 }
 
 T.get('search/tweets', params, function(err, data, response) {
+  "use strict";
   if(!err){
-    // This is where the magic will happen
+    // Loop through the returned tweets
+        for(let i = 0; i < data.statuses.length; i++){
+          // Get the tweet Id from the returned data
+          let id = { id: data.statuses[i].id_str }
+          // Try to Favorite the selected Tweet
+          T.post('favorites/create', id, function(err, response){
+            // If the favorite fails, log the error message
+            if(err){
+              console.log(err[0].message);
+            }
+            // If the favorite is successful, log the url of the tweet
+            else{
+              let username = response.user.screen_name;
+              let tweetId = response.id_str;
+              console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+            }
+          });
+        }
   } else {
     console.log(err);
   }
