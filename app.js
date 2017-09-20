3293a07a9438853c1633cea5f4e55c56cc9e5c87
David Cisneros <d.cisneros849@gmail.com>
2017-09-20 10:26:22 -0400

    added get request for tweets

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ var Twitter = require('twitter');
 var config = require('./config.js');
 
 var T = new Twitter(config);
+
+// Set up your search parameters
+var params = {
+  q: '#wordpress',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+T.get('search/tweets', params, function(err, data, response) {
+  if(!err){
+    // This is where the magic will happen
+  } else {
+    console.log(err);
+  }
+})
